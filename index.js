b6da3bc8fec03fb9d6299c7f63592a571a77dd25
Epoza <86025026+Epoza@users.noreply.github.com>
2024-04-27 23:54:30 -0400

    add more functions

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+console.log("sum range:")
 const sumRange = (num) => {
     if(num == 0){
         return 0;
@@ -6,6 +7,7 @@ const sumRange = (num) => {
 };
 console.log(sumRange(10));
 
+console.log("power:")
 const power = (base, exponent) => {
     if (exponent == 0){
         return 1
@@ -14,6 +16,7 @@ const power = (base, exponent) => {
 }
 console.log(power(2, 3))
 
+console.log("factorial:")
 const factorial =  (num) => {
     if (num == 0){
         return 1
@@ -21,3 +24,32 @@ const factorial =  (num) => {
     return num * factorial(num - 1)
 };
 console.log(factorial(5));
+
+console.log("product of array:")
+const productOfArray = (arr) => {
+    if(arr.length === 0) {
+        return 1
+    }
+    return arr.shift() * productOfArray(arr);
+}
+console.log(productOfArray([1, 4, 5, 10]));
+
+console.log("sumSquares")
+const sumSquares = (arr) => {
+    if (arr.length === 0) {
+        return 0;
+    }
+    let total = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        if (typeof arr[i] === 'object' && arr[i] !== null) {
+            total += sumSquares(arr[i]);
+        } else {
+            total += arr[i] * arr[i];
+        }
+
+    }
+    return total;
+}
+
+console.log(sumSquares([[4, 4], [2, 4, 5, [3]]]));
